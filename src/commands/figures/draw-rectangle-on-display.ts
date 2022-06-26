0a8781c230485ba52cbcc4b9695784007f89b0c0
toshaso <togzhan.com@mail.ru>
2022-06-27 00:17:48 +0600

    feat: remove useless drawer

--- src/commands/figures/draw-rectangle-on-display.ts
+++ src/commands/figures/draw-rectangle-on-display.ts
@@ -5,7 +5,6 @@ export function drawRectangleOnDisplay(command: string, [length, width]: string[
     moveMouseInDirection('mouse_right', [width]);
     moveMouseInDirection('mouse_up', [length]);
     moveMouseInDirection('mouse_left', [width]);
-    moveMouseInDirection('mouse_down', ['20']);
 
     return command + length + ',' + width;
 }
