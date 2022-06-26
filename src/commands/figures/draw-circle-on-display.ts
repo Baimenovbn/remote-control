1c6d5bfd9d9fe011ed768314a68542891a46993d
toshaso <togzhan.com@mail.ru>
2022-06-27 00:17:30 +0600

    feat: add mouse click togglers

--- src/commands/figures/draw-circle-on-display.ts
+++ src/commands/figures/draw-circle-on-display.ts
@@ -4,6 +4,7 @@ export function drawCircleOnDisplay(command: string, [radius]: string[]) {
     const { x, y } = robot.getMousePos();
 
     robot.moveMouse(x, y);
+    robot.setMouseDelay(0.9);
     drawCircle(+radius);
     return command + radius;
 }
@@ -11,10 +12,13 @@ export function drawCircleOnDisplay(command: string, [radius]: string[]) {
 const drawCircle = (radius: number) => {
     const mousePos = robot.getMousePos();
 
+    robot.mouseToggle('up');
     for (let i = 0; i <= Math.PI * 2; i += 0.01) {
+
         const x = mousePos.x + (radius * Math.cos(i));
         const y = mousePos.y + (radius * Math.sin(i));
 
         robot.dragMouse(x, y);
+        robot.mouseToggle('down');
     }
 }
--- src/utils/commands-handler.ts
+++ src/utils/commands-handler.ts
@@ -1,14 +1,17 @@
 import {WebSocket} from "ws";
 import { commandsMapper } from "../commands/commands-mapper";
+import robot from "robotjs";
 
 export const commandsHandler = async (commandWithArgs: string, ws: WebSocket) => {
     const [command, ...args] = commandWithArgs.split(' ');
 
     const action = commandsMapper[command];
     if (action) {
+        robot.mouseToggle('down');
         const result = await action(command, args);
+        robot.mouseToggle('up');
 
-        ws.send(result + '\0');
+        ws.send(result);
     }
 }
 
